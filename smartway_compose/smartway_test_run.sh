c52ad061108092c58b356a5c36f1a8c60fee81ff
Hatenickname <mycoupe058@gmail.com>
2022-06-24 11:08:47 +0300

    Update smartway_test_run.sh

--- smartway_compose/smartway_test_run.sh
+++ smartway_compose/smartway_test_run.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli docker-compose-plugin docker-compose
+sudo apt-get install docker-ce docker-ce-cli docker-compose-plugin docker-compose containerd.io
 sudo docker-compose up
